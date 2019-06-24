94acf2d205b260a6286e376dd214621707ba50a8
apanada <ajit.panada@outlook.com>
2019-06-25 01:10:22 +0530

    Added Video Module

--- src/app/components/product/product-home/product-home.component.ts
+++ src/app/components/product/product-home/product-home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { EmbedVideoService } from 'ngx-embed-video';
 
 @Component({
   selector: 'app-product-home',
@@ -6,10 +7,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product-home.component.scss']
 })
 export class ProductHomeComponent implements OnInit {
+  panelOpenState = false;
 
-  constructor() { }
+  yt_iframe_html: any;
+  vimeo_iframe_html: any;
+  dm_iframe_html: any;
+
+  vimeoUrl = "https://vimeo.com/197933516";
+  youtubeUrl = "https://www.youtube.com/watch?v=iHhcHTlGtRs";
+  dailymotionUrl = "https://www.dailymotion.com/video/x20qnej_red-bull-presents-wild-ride-bmx-mtb-dirt_sport";
+
+  constructor(private embedService: EmbedVideoService) { }
 
   ngOnInit() {
+    this.yt_iframe_html = this.embedService.embed(this.youtubeUrl);
+    this.vimeo_iframe_html = this.embedService.embed(this.vimeoUrl);
+    this.dm_iframe_html = this.embedService.embed(this.dailymotionUrl);
   }
-
 }
--- src/app/components/product/product.module.ts
+++ src/app/components/product/product.module.ts
@@ -1,20 +1,28 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { EmbedVideo } from 'ngx-embed-video';
 
 import { ProductRoutingModule } from './product-routing.module';
 import { ProductHomeComponent } from './product-home/product-home.component';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import { MatButtonToggleModule } from '@angular/material/button-toggle';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [ProductHomeComponent],
   imports: [
     CommonModule,
+    HttpClientModule,
     ProductRoutingModule,
     MatCardModule,
     MatButtonModule,
-    MatButtonToggleModule
+    MatButtonToggleModule,
+    MatExpansionModule,
+    FlexLayoutModule,
+    EmbedVideo
   ]
 })
 export class ProductModule { }
